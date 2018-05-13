971b7999ff701ca6c0b13d39c82f2ffe86aa4977
Toshinori Sugita <s.toshi0607@gmail.com>
2018-05-13 17:09:32 +0900

    bump up to 0.1.1

--- gig.rb
+++ gig.rb
@@ -1,22 +1,22 @@
 class Gig < Formula
   homepage 'https://github.com/toshi0607/gig'
-  version '0.0.1'
+  version '0.1.1'
 
   if Hardware::CPU.is_32_bit?
     if OS.linux?
-      url 'https://github.com/toshi0607/gig/releases/download/v0.0.1/gig_linux_386.zip'
-      sha256 'decc058338faccf9346c3293cb431ea1fbb5061e1626eedbf046f00d6f54d8ba'
+      url 'https://github.com/toshi0607/gig/releases/download/v0.1.1/gig_linux_386.zip'
+      sha256 '7614f16e2644f395de62d33815c6a274577fe4dedf73560b24b6450a9ea812dd'
     else
-      url 'https://github.com/toshi0607/gig/releases/download/v0.0.1/gig_darwin_386.zip'
-      sha256 'a5520b8832c6451c6d16305b0ae8f2531137dc57c026a7d5e04f3e1b97b41eac'
+      url 'https://github.com/toshi0607/gig/releases/download/v0.1.1/gig_darwin_386.zip'
+      sha256 'b37ef220c0882e210b2080d64a05a2b48dec80013001fce64b6dca4c3211192b'
     end
   else
     if OS.linux?
-      url 'https://github.com/toshi0607/gig/releases/download/v0.0.1/gig_linux_amd64.zip'
-      sha256 '3ea541a13fcfc13c1d3ca9161621aef2903027b710125e408277e28c2c2a647a'
+      url 'https://github.com/toshi0607/gig/releases/download/v0.1.1/gig_linux_amd64.zip'
+      sha256 'a80a37dfec4e0cdf590a92ef449b06e0ea1e837e37137e738ba440e9720f7684'
     else
-      url 'https://github.com/toshi0607/gig/releases/download/v0.0.1/gig_darwin_amd64.zip'
-      sha256 '45c214f9d9747a9e1eef9aa5eda61839ec2e41ec2113cb9773648ec42ac4b3ed'
+      url 'https://github.com/toshi0607/gig/releases/download/v0.1.1/gig_darwin_amd64.zip'
+      sha256 '36a001851cf6a7a8f45a34a458357392a44fd19c3e1f50168b774fd921526051'
     end
   end
 
